49b9d54d2da3a7295dffe185f859564493d85eec
Rafael Fernandes <luizrafael@gmail.com>
2021-01-04 12:41:23 -0500

    Update server.js

--- accounts/server.js
+++ accounts/server.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 require('dotenv').config({silent: true, path: `${__dirname}/.env`});
 
 var server = require('./app');
-var port = 3400;
+var port = process.env.PORT;
 
-console.log(`Running on ${process.env.BASE_PATH}:${process.env.PORT}, connecting to ${process.env.MONGO_URL}`)
+console.log(`Running on ${process.env.BASE_PATH}:${port}, connecting to ${process.env.MONGO_URL}`)
 
 mongoose.connect(process.env.MONGO_URL, function (ignore, connection) {
     connection.onOpen();
